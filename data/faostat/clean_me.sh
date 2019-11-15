07f67d82c4ece23032e861552d8891c0079e1971
Martin Kostelansky <kostelansky17@github.com>
2019-11-15 16:43:35 +0100

    comment and describe usage

--- data/faostat/clean_me.sh
+++ data/faostat/clean_me.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+# Run the script and pass relative path to documnet
+# Example: './clean_me.sh FAOSTAT_file_\(Normalized\).csv'
+# The script will return cleared file named 'FAOSTAT_file_cleared.csv'
+# WARNING: THE ORIGINAL DATAFILE WILL BE DELETED AFTER PROCESSING!!!
 
 if [ "$#" -ne 1 ]; then
-    echo "Illegal number of parameters"
+    echo "Illegal number of parameters, please pass the path to the file to be processed"
     exit
 fi
 
